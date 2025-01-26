4ff9d898bac5f6fc0bc292b7102f40ef45524292
sihyeong <syhy7051@gmail.com>
2025-01-26 17:49:12 +0900

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,11 @@
 import type { Config } from "tailwindcss";
-
+import daisyui from "daisyui";
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  theme: {},
+  plugins: [daisyui],
 } satisfies Config;
